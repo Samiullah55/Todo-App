94c613d85d75745458b9368f395c36105712bb38
Sami Ullah <samibangash2018@gmail.com>
2023-04-26 13:25:40 +0500

    changes made in the files

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { List,ListItem,ListItemText,ListItemAvatar } from '@mui/material';
+import { List,ListItem,ListItemText,ListItemAvatar,Button } from '@mui/material';
 import "../css/todo.css";
-
+import { db } from '../firebase';
 
 function Todos(props) {
   return (
@@ -11,6 +11,7 @@ function Todos(props) {
         </ListItemAvatar>
         <ListItemText primary="Todo" secondary={props.todo}/>
       </ListItem>
+      <Button onClick={(e)=>db.collection('Todos').doc(props.todo.id).delete()}>Delete</Button>
 
     </List>
         
